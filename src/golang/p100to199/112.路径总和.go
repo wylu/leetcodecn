3b8e02d8fe0baf3f28a7933a79eaaf7a44cf3793
wylu <158677478@qq.com>
2020-07-07 07:58:05 +0800

    路径总和

---
+++ src/golang/p100to199/112.路径总和.go
@@ -0,0 +1,66 @@
+package p100to199
+
+/*
+ * @lc app=leetcode.cn id=112 lang=golang
+ *
+ * [112] 路径总和
+ *
+ * https://leetcode-cn.com/problems/path-sum/description/
+ *
+ * algorithms
+ * Easy (49.90%)
+ * Likes:    335
+ * Dislikes: 0
+ * Total Accepted:    89.8K
+ * Total Submissions: 179.7K
+ * Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,null,1]\n22'
+ *
+ * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例:
+ * 给定如下二叉树，以及目标和 sum = 22，
+ *
+ * ⁠             5
+ * ⁠            / \
+ * ⁠           4   8
+ * ⁠          /   / \
+ * ⁠         11  13  4
+ * ⁠        /  \      \
+ * ⁠       7    2      1
+ *
+ *
+ * 返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+ *
+ */
+
+/**
+ * @File    :   112.路径总和.go
+ * @Time    :   2020/07/07 07:46:34
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left == nil && root.Right == nil {
+		return root.Val == sum
+	}
+	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
+}
+
+// @lc code=end
