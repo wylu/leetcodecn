c2c5014066bb5bf16203a8b32c578b92c7318c3d
wylu <158677478@qq.com>
2020-07-06 23:11:18 +0800

    斐波那契数列

---
+++ src/golang/offer/10.斐波那契数列.go
@@ -0,0 +1,22 @@
+package offer
+
+/**
+ * @File    :   10.斐波那契数列.go
+ * @Time    :   2020/07/06 23:07:34
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+func fib(n int) int {
+	mod := 1000000007
+	f := [2]int{0, 1}
+	if n < 2 {
+		return f[n]
+	}
+	for i := 2; i <= n; i++ {
+		f[0], f[1] = f[1], (f[0]+f[1])%mod
+	}
+	return f[1]
+}
