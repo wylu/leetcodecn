0992cdaed9d0cf9b6f15f92e0441e4fc212ef679
wylu <158677478@qq.com>
2020-07-08 23:44:24 +0800

    删除链表的节点

---
+++ src/golang/offer/18.删除链表的节点.go
@@ -0,0 +1,35 @@
+package offer
+
+/**
+ * @File    :   18.删除链表的节点.go
+ * @Time    :   2020/07/08 23:34:15
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteNode(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	dummy := &ListNode{Next: head}
+	cur := dummy
+	for cur.Next != nil {
+		if cur.Next.Val == val {
+			cur.Next = cur.Next.Next
+			break
+		}
+		cur = cur.Next
+	}
+	return dummy.Next
+}
