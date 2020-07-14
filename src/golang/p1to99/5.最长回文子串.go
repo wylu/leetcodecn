3eaa99a129248f82d9a07c1d5e809abe53cb8f8b
wylu <158677478@qq.com>
2020-07-14 23:06:11 +0800

    最长回文子串

---
+++ src/golang/p1to99/5.最长回文子串.go
@@ -0,0 +1,72 @@
+package p1to99
+
+/*
+ * @lc app=leetcode.cn id=5 lang=golang
+ *
+ * [5] 最长回文子串
+ *
+ * https://leetcode-cn.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (30.91%)
+ * Likes:    2423
+ * Dislikes: 0
+ * Total Accepted:    316.3K
+ * Total Submissions: 1M
+ * Testcase Example:  '"babad"'
+ *
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ *
+ * 示例 1：
+ *
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ *
+ *
+ * 示例 2：
+ *
+ * 输入: "cbbd"
+ * 输出: "bb"
+ *
+ *
+ */
+
+/**
+ * @File    :   5.最长回文子串.go
+ * @Time    :   2020/07/14 22:51:51
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+// @lc code=start
+func longestPalindrome(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+
+	start, end := 0, 0
+	for i := 0; i < len(s); i++ {
+		l1, r1 := expandAroundCenter(s, i, i)
+		l2, r2 := expandAroundCenter(s, i, i+1)
+		if r1-l1 > end-start {
+			start, end = l1, r1
+		}
+		if r2-l2 > end-start {
+			start, end = l2, r2
+		}
+	}
+	return s[start : end+1]
+}
+
+func expandAroundCenter(s string, left, right int) (int, int) {
+	for left >= 0 && right < len(s) && s[left] == s[right] {
+		left--
+		right++
+	}
+	return left + 1, right - 1
+}
+
+// @lc code=end
