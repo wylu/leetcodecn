d1df3def0c27d2716a6456bbdf6864cbe6b535bc
wylu <158677478@qq.com>
2020-07-17 00:35:46 +0800

    搜索插入位置

---
+++ src/golang/p1to99/35.搜索插入位置.go
@@ -0,0 +1,77 @@
+package p1to99
+
+/*
+ * @lc app=leetcode.cn id=35 lang=golang
+ *
+ * [35] 搜索插入位置
+ *
+ * https://leetcode-cn.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (45.74%)
+ * Likes:    565
+ * Dislikes: 0
+ * Total Accepted:    184.4K
+ * Total Submissions: 402.7K
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ *
+ * 你可以假设数组中无重复元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ *
+ *
+ * 示例 3:
+ *
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ *
+ *
+ * 示例 4:
+ *
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ *
+ *
+ */
+
+/**
+ * @File    :   35.搜索插入位置.go
+ * @Time    :   2020/07/17 00:22:56
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+// @lc code=start
+func searchInsert(nums []int, target int) int {
+	if nums == nil {
+		return -1
+	}
+
+	lo, hi := 0, len(nums)-1
+	for lo <= hi {
+		mid := lo + (hi-lo)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			lo = mid + 1
+		} else {
+			hi = mid - 1
+		}
+	}
+	return lo
+}
+
+// @lc code=end
---
+++ src/golang/p1to99/35.搜索插入位置_test.go
@@ -0,0 +1,59 @@
+package p1to99
+
+import "testing"
+
+func Test_searchInsert(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// Case 1
+		{
+			name: "Case 1",
+			args: args{
+				nums:   []int{1, 3, 5, 6},
+				target: 5,
+			},
+			want: 2,
+		},
+		// Case 2
+		{
+			name: "Case 2",
+			args: args{
+				nums:   []int{1, 3, 5, 6},
+				target: 2,
+			},
+			want: 1,
+		},
+		// Case 3
+		{
+			name: "Case 3",
+			args: args{
+				nums:   []int{1, 3, 5, 6},
+				target: 7,
+			},
+			want: 4,
+		},
+		// Case 4
+		{
+			name: "Case 4",
+			args: args{
+				nums:   []int{1, 3, 5, 6},
+				target: 0,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := searchInsert(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("searchInsert() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
