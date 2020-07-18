c2bf63faf64604519707caf4f09619e233c18cdf
wylu <158677478@qq.com>
2020-07-18 13:43:20 +0800

    爬楼梯

---
+++ src/golang/p1to99/70.爬楼梯.go
@@ -0,0 +1,66 @@
+package p1to99
+
+/*
+ * @lc app=leetcode.cn id=70 lang=golang
+ *
+ * [70] 爬楼梯
+ *
+ * https://leetcode-cn.com/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (49.87%)
+ * Likes:    1136
+ * Dislikes: 0
+ * Total Accepted:    243K
+ * Total Submissions: 485.8K
+ * Testcase Example:  '2'
+ *
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ * 注意：给定 n 是一个正整数。
+ *
+ * 示例 1：
+ *
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ *
+ * 示例 2：
+ *
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ *
+ *
+ */
+
+/**
+ * @File    :   70.爬楼梯.go
+ * @Time    :   2020/07/18 13:40:18
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+// @lc code=start
+func climbStairs(n int) int {
+	f := [2]int{1, 1}
+	if n < 2 {
+		return f[n]
+	}
+
+	for i := 2; i <= n; i++ {
+		f[0], f[1] = f[1], f[0]+f[1]
+	}
+	return f[1]
+}
+
+// @lc code=end
