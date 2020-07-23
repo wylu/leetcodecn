3462300f9d31b338ff12b681e06fdaa9bb0c18c8
wylu <158677478@qq.com>
2020-07-24 07:45:42 +0800

    买卖股票的最佳时机II

---
+++ src/cpp/explore/topquestions/easy/array/买卖股票的最佳时机II.cpp
@@ -0,0 +1,25 @@
+/**
+ * @File    :   买卖股票的最佳时机II.cpp
+ * @Time    :   2020/07/24 07:40:05
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] - prices[i - 1] > 0) {
+                ans += prices[i] - prices[i - 1];
+            }
+        }
+        return ans;
+    }
+};
