36fb06649a4a46ed94087c5fe96cca726a90a16c
wylu <158677478@qq.com>
2020-07-25 11:43:47 +0800

    只出现一次的数字

--- src/cpp/explore/topquestions/easy/array/只出现一次的数字.cpp
+++ src/cpp/explore/topquestions/easy/array/只出现一次的数字.cpp
@@ -8,7 +8,10 @@ using namespace std;
  * @Version :   1.0
  * @Contact :   15wylu@gmail.com
  * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
- * @Desc    :   0 ^ n = n,  n ^ n = n
+ * @Desc    :   https://leetcode-cn.com/explore/featured/card/top-interview-questions-easy/1/array/25/
+ * 
+ * 0 ^ n = n,  n ^ n = n
+ * 
  */
 class Solution {
 public:
