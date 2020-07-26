5fa93ffc86760e1cc2e7509688582a7a843b7f34
wylu <158677478@qq.com>
2020-07-26 10:27:19 +0800

    移动零

--- src/python/explore/topquestions/easy/array/移动零.py
+++ src/python/explore/topquestions/easy/array/移动零.py
@@ -14,10 +14,8 @@ from typing import List
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
         slow, fast = 0, 0
+
         while fast < len(nums):
             if nums[fast] != 0:
                 nums[slow], nums[fast] = nums[fast], nums[slow]
