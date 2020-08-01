5faa865aabcfb61571b4eb3ee3deebbf5ad23e5d
wylu <158677478@qq.com>
2020-08-01 22:19:20 +0800

    合并两个有序链表

---
+++ src/python/explore/topquestions/easy/linkedlist/合并两个有序链表.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   合并两个有序链表.py
+@Time    :   2020/08/01 22:11:35
+@Author  :   wylu
+@Version :   1.0
+@Contact :   15wylu@gmail.com
+@License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+@Desc    :
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1 and not l2:
+            return
+
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+
+        dummy = ListNode(0)
+        cur = dummy
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+
+        cur.next = l1 if l1 else l2
+        return dummy.next
