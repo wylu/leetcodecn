31caf3d2a92e7b67d4b85406576289d70c702de9
wylu <158677478@qq.com>
2020-08-04 13:50:46 +0800

    preorder traversal

---
+++ src/python/learn/binarytree/traverse/preorder.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   preorder.py
+@Time    :   2020/08/04 12:35:43
+@Author  :   wylu
+@Version :   1.0
+@Contact :   15wylu@gmail.com
+@License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+@Desc    :
+"""
+import sys
+
+from typing import List
+
+sys.path.append('../../..')
+
+from common.treenode import TreeNode
+
+
+class PreorderTraversal(object):
+    @classmethod
+    def recursive(cls, root: TreeNode, ans: List[int]) -> None:
+
+        pass
