54ca240e90ab7275047a3e8bf2e8e154bba6e071
wylu <158677478@qq.com>
2020-08-06 23:23:17 +0800

    二叉树展开为链表

---
+++ src/golang/p100to199/114.二叉树展开为链表.go
@@ -0,0 +1,89 @@
+package p100to199
+
+/*
+ * @lc app=leetcode.cn id=114 lang=golang
+ *
+ * [114] 二叉树展开为链表
+ *
+ * https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/description/
+ *
+ * algorithms
+ * Medium (70.91%)
+ * Likes:    499
+ * Dislikes: 0
+ * Total Accepted:    73.9K
+ * Total Submissions: 104.2K
+ * Testcase Example:  '[1,2,5,3,4,null,6]'
+ *
+ * 给定一个二叉树，原地将它展开为一个单链表。
+ *
+ *
+ *
+ * 例如，给定二叉树
+ *
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   5
+ * ⁠/ \   \
+ * 3   4   6
+ *
+ * 将其展开为：
+ *
+ * 1
+ * ⁠\
+ * ⁠ 2
+ * ⁠  \
+ * ⁠   3
+ * ⁠    \
+ * ⁠     4
+ * ⁠      \
+ * ⁠       5
+ * ⁠        \
+ * ⁠         6
+ *
+ */
+
+/**
+ * @File    :   114.二叉树展开为链表.go
+ * @Time    :   2020/08/06 23:13:44
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func flatten(root *TreeNode) {
+	if root == nil {
+		return
+	}
+
+	for root != nil {
+		if root.Left == nil {
+			root = root.Right
+			continue
+		}
+
+		// 找到左子树的最右结点
+		pre := root.Left
+		for pre.Right != nil {
+			pre = pre.Right
+		}
+
+		pre.Right = root.Right
+		root.Right = root.Left
+		root.Left = nil
+		root = root.Right
+	}
+}
+
+// @lc code=end
