7593b390878dba30efb1446ca358e61f188bc557
wylu <158677478@qq.com>
2020-08-14 23:47:16 +0800

    二叉树的最大深度

---
+++ src/python/explore/binarytree/exercise/二叉树的最大深度.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   二叉树的最大深度.py
+@Time    :   2020/08/14 23:44:46
+@Author  :   wylu
+@Version :   1.0
+@Contact :   15wylu@gmail.com
+@License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+@Desc    :
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
