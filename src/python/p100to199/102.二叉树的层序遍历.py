85bc2a68f5a5dec469a5534fcf05826493d2e1dd
wylu <158677478@qq.com>
2020-08-14 23:44:06 +0800

    二叉树的层序遍历

--- src/python/p100to199/102.二叉树的层序遍历.py
+++ src/python/p100to199/102.二叉树的层序遍历.py
@@ -1,14 +1,56 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-@File    :   二叉树的层序遍历.py
-@Time    :   2020/08/14 23:16:43
+@File    :   102.二叉树的层序遍历.py
+@Time    :   2020/08/14 23:35:20
 @Author  :   wylu
 @Version :   1.0
 @Contact :   15wylu@gmail.com
 @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
 @Desc    :
 """
+
+#
+# @lc app=leetcode.cn id=102 lang=python3
+#
+# [102] 二叉树的层序遍历
+#
+# https://leetcode-cn.com/problems/binary-tree-level-order-traversal/description/
+#
+# algorithms
+# Medium (63.18%)
+# Likes:    596
+# Dislikes: 0
+# Total Accepted:    177.6K
+# Total Submissions: 281.1K
+# Testcase Example:  '[3,9,20,null,null,15,7]'
+#
+# 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+#
+#
+#
+# 示例：
+# 二叉树：[3,9,20,null,null,15,7],
+#
+# ⁠   3
+# ⁠  / \
+# ⁠ 9  20
+# ⁠   /  \
+# ⁠  15   7
+#
+#
+# 返回其层次遍历结果：
+#
+# [
+# ⁠ [3],
+# ⁠ [9,20],
+# ⁠ [15,7]
+# ]
+#
+#
+#
+
+# @lc code=start
 from collections import deque
 from typing import List
 
@@ -47,31 +89,4 @@ class Solution:
         return ans
 
 
-# class Solution:
-#     def levelOrder(self, root: TreeNode) -> List[List[int]]:
-#         if not root:
-#             return []
-
-#         ans = []
-#         cur_tail, next_tail, level = root, None, []
-#         que = deque()
-#         que.append(root)
-
-#         while que:
-#             node = que.popleft()
-#             level.append(node.val)
-
-#             if node.left:
-#                 next_tail = node.left
-#                 que.append(node.left)
-
-#             if node.right:
-#                 next_tail = node.right
-#                 que.append(node.right)
-
-#             if node == cur_tail:
-#                 cur_tail = next_tail
-#                 ans.append(level)
-#                 level = []
-
-#         return ans
+# @lc code=end
