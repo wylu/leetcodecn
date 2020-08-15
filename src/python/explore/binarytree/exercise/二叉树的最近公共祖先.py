07dab3250fec8b726e562bab2b9ad5480530ce53
wylu <158677478@qq.com>
2020-08-15 19:58:52 +0800

    二叉树的最近公共祖先

---
+++ src/python/explore/binarytree/exercise/二叉树的最近公共祖先.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   二叉树的最近公共祖先.py
+@Time    :   2020/08/15 19:34:15
+@Author  :   wylu
+@Version :   1.0
+@Contact :   15wylu@gmail.com
+@License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+@Desc    :
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode',
+                             q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+
+        return left or right
