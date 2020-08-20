a9571cefc7feb227dfe55e59d61924d56edfdeef
wylu <158677478@qq.com>
2020-08-20 23:51:28 +0800

    正则表达式匹配

--- src/python/offer/19.正则表达式匹配.py
+++ src/python/offer/19.正则表达式匹配.py
@@ -1,90 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-@File    :   10.正则表达式匹配.py
-@Time    :   2020/08/12 23:32:47
+@File    :   19.正则表达式匹配.py
+@Time    :   2020/08/20 23:34:31
 @Author  :   wylu
 @Version :   1.0
 @Contact :   15wylu@gmail.com
 @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
 @Desc    :
-"""
-
-#
-# @lc app=leetcode.cn id=10 lang=python3
-#
-# [10] 正则表达式匹配
-#
-# https://leetcode-cn.com/problems/regular-expression-matching/description/
-#
-# algorithms
-# Hard (30.13%)
-# Likes:    1449
-# Dislikes: 0
-# Total Accepted:    106.7K
-# Total Submissions: 354K
-# Testcase Example:  '"aa"\n"a"'
-#
-# 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
-#
-# '.' 匹配任意单个字符
-# '*' 匹配零个或多个前面的那一个元素
-#
-#
-# 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
-#
-# 说明:
-#
-#
-# s 可能为空，且只包含从 a-z 的小写字母。
-# p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。
-#
-#
-# 示例 1:
-#
-# 输入:
-# s = "aa"
-# p = "a"
-# 输出: false
-# 解释: "a" 无法匹配 "aa" 整个字符串。
-#
-#
-# 示例 2:
-#
-# 输入:
-# s = "aa"
-# p = "a*"
-# 输出: true
-# 解释: 因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
-#
-#
-# 示例 3:
-#
-# 输入:
-# s = "ab"
-# p = ".*"
-# 输出: true
-# 解释: ".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
-#
-#
-# 示例 4:
-#
-# 输入:
-# s = "aab"
-# p = "c*a*b"
-# 输出: true
-# 解释: 因为 '*' 表示零个或多个，这里 'c' 为 0 个, 'a' 被重复一次。因此可以匹配字符串 "aab"。
-#
-#
-# 示例 5:
-#
-# 输入:
-# s = "mississippi"
-# p = "mis*is*p*."
-# 输出: false
-#
-#
-"""
 动态规划
 
 思路与算法:
@@ -218,11 +141,8 @@ P = ab*d*c
 """
 
 
-# @lc code=start
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        m, n = len(s), len(p)
-
         def matches(x: int, y: int) -> bool:
             if x < 0:
                 return False
@@ -230,6 +150,7 @@ class Solution:
                 return True
             return s[x] == p[y]
 
+        m, n = len(s), len(p)
         f = [[False] * (n + 1) for _ in range(m + 1)]
         f[0][0] = True
 
@@ -244,6 +165,3 @@ class Solution:
                         f[i][j] |= f[i - 1][j - 1]
 
         return f[m][n]
-
-
-# @lc code=end
