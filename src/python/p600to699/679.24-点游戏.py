df7593c426342ffb955a61a66ffa752366633f96
wylu <158677478@qq.com>
2020-08-22 21:17:00 +0800

    24-点游戏

--- src/python/p600to699/679.24-点游戏.py
+++ src/python/p600to699/679.24-点游戏.py
@@ -89,7 +89,45 @@ from typing import List
 # @lc code=start
 class Solution:
     def judgePoint24(self, nums: List[int]) -> bool:
-        pass
+        TARGET = 24
+        EPSILON = 1e-6
+        ADD, MUL, SUB, DIV = 0, 1, 2, 3
+
+        def dfs(nums: List[int]) -> bool:
+            if not nums:
+                return False
+            if len(nums) == 1:
+                return abs(nums[0] - TARGET) < EPSILON
+
+            for i, x in enumerate(nums):
+                for j, y in enumerate(nums):
+                    if i != j:
+                        newNums = []
+                        for k, z in enumerate(nums):
+                            if k != i and k != j:
+                                newNums.append(z)
+
+                        for k in range(4):
+                            if k < 2 and i > j:
+                                continue
+
+                            if k == ADD:
+                                newNums.append(x + y)
+                            elif k == MUL:
+                                newNums.append(x * y)
+                            elif k == SUB:
+                                newNums.append(x - y)
+                            elif k == DIV:
+                                if abs(y) < EPSILON:
+                                    continue
+                                newNums.append(x / y)
+
+                            if dfs(newNums):
+                                return True
+
+                            newNums.pop()
+
+        return dfs(nums)
 
 
 # @lc code=end
