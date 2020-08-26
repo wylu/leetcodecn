f094e161089153c09d2194ce404f4822869ef0f8
wylu <158677478@qq.com>
2020-08-26 22:34:43 +0800

    最长递增子序列的个数

--- src/python/p600to699/673.最长递增子序列的个数.py
+++ src/python/p600to699/673.最长递增子序列的个数.py
@@ -47,6 +47,13 @@
 #
 #
 from typing import List
+"""
+Dynamic Programming
+
+State:
+  dp[i]: 表示以 nums[i] 为结尾的最长递增子序列的长度
+  cnts[i]: 表示以 nums[i] 为结尾且长度为 dp[i] 的递增子序列的个数
+"""
 
 
 # @lc code=start
@@ -55,15 +62,29 @@ class Solution:
         if not nums:
             return 0
 
-        n, m = len(nums), 0
-        dp = [1] * n
+        n, m = len(nums), 1
+        dp, cnts = [1] * n, [1] * n
+
         for i in range(1, n):
-            for j in range(0, i):
-                if nums[i] > nums[i - 1]:
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    if dp[j] + 1 > dp[i]:
+                        cnts[i] = cnts[j]
+                    elif dp[j] + 1 == dp[i]:
+                        cnts[i] += cnts[j]
+
                     dp[i] = max(dp[i], dp[j] + 1)
+
             m = max(m, dp[i])
 
-        return sum(1 for v in dp if v == m)
+        return sum(cnts[i] for i, v in enumerate(dp) if v == m)
 
 
 # @lc code=end
+
+if __name__ == '__main__':
+    solu = Solution()
+    print(solu.findNumberOfLIS([1, 2, 4, 3, 5, 4, 7, 2]))
+    print(solu.findNumberOfLIS([1, 3, 5, 4, 7]))
+    print(solu.findNumberOfLIS([2, 2, 2, 2, 2]))
+    print(solu.findNumberOfLIS([1]))
