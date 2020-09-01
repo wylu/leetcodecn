d2e4aca7fc2e629bc1ac5b8c8b6f805d91d7fcbc
wylu <158677478@qq.com>
2020-09-02 00:06:59 +0800

    二叉树中和为某一值的路径

--- src/python/offer/34.二叉树中和为某一值的路径.py
+++ src/python/offer/34.二叉树中和为某一值的路径.py
@@ -1,55 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-@File    :   113.路径总和-ii.py
-@Time    :   2020/09/01 23:51:19
+@File    :   34.二叉树中和为某一值的路径.py
+@Time    :   2020/09/02 00:00:50
 @Author  :   wylu
 @Version :   1.0
 @Contact :   15wylu@gmail.com
 @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
 @Desc    :
 """
-
-#
-# @lc app=leetcode.cn id=113 lang=python3
-#
-# [113] 路径总和 II
-#
-# https://leetcode-cn.com/problems/path-sum-ii/description/
-#
-# algorithms
-# Medium (60.91%)
-# Likes:    306
-# Dislikes: 0
-# Total Accepted:    70.1K
-# Total Submissions: 115K
-# Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,5,1]\n22'
-#
-# 给定一个二叉树和一个目标和，找到所有从根节点到叶子节点路径总和等于给定目标和的路径。
-#
-# 说明: 叶子节点是指没有子节点的节点。
-#
-# 示例:
-# 给定如下二叉树，以及目标和 sum = 22，
-#
-# ⁠             5
-# ⁠            / \
-# ⁠           4   8
-# ⁠          /   / \
-# ⁠         11  13  4
-# ⁠        /  \    / \
-# ⁠       7    2  5   1
-#
-#
-# 返回:
-#
-# [
-# ⁠  [5,4,11,2],
-# ⁠  [5,8,4,5]
-# ]
-#
-#
-#
 from typing import List
 
 
@@ -61,7 +20,6 @@ class TreeNode:
         self.right = None
 
 
-# @lc code=start
 class Solution:
     def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
         if not root:
@@ -83,6 +41,3 @@ class Solution:
         ans, stack = [], []
         dfs(root, sum)
         return ans
-
-
-# @lc code=end
