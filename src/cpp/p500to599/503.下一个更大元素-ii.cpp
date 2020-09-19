25e22bafdfca09910e21f15608a87870958c698d
wylu <158677478@qq.com>
2020-09-19 12:14:49 +0800

    下一个更大元素-ii

---
+++ src/cpp/p500to599/503.下一个更大元素-ii.cpp
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=503 lang=cpp
+ *
+ * [503] 下一个更大元素 II
+ *
+ * https://leetcode-cn.com/problems/next-greater-element-ii/description/
+ *
+ * algorithms
+ * Medium (57.36%)
+ * Likes:    186
+ * Dislikes: 0
+ * Total Accepted:    30.1K
+ * Total Submissions: 52.5K
+ * Testcase Example:  '[1,2,1]'
+ *
+ * 给定一个循环数组（最后一个元素的下一个元素是数组的第一个元素），输出每个元素的下一个更大元素。数字 x
+ * 的下一个更大的元素是按数组遍历顺序，这个数字之后的第一个比它更大的数，这意味着你应该循环地搜索它的下一个更大的数。如果不存在，则输出 -1。
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: [1,2,1]
+ * 输出: [2,-1,2]
+ * 解释: 第一个 1 的下一个更大的数是 2；
+ * 数字 2 找不到下一个更大的数； 
+ * 第二个 1 的下一个最大的数需要循环搜索，结果也是 2。
+ * 
+ * 
+ * 注意: 输入数组的长度不会超过 10000。
+ * 
+ */
+
+/**
+ * @File    :   503.下一个更大元素-ii.cpp
+ * @Time    :   2020/09/19 11:13:02
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, -1);
+        stack<int> s;
+        for (int i = 0; i < 2 * n; i++) {
+            while (!s.empty() && nums[i % n] > nums[s.top()]) {
+                ans[s.top()] = nums[i % n];
+                s.pop();
+            }
+            s.push(i % n);
+        }
+        return ans;
+    }
+};
+// @lc code=end
