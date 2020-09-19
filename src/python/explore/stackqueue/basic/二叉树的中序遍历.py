e50efa490cfd1292f5d3f62c4fa17b48c3477496
wylu <158677478@qq.com>
2020-09-19 19:02:48 +0800

    二叉树的中序遍历

---
+++ src/python/explore/stackqueue/basic/二叉树的中序遍历.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   二叉树的中序遍历.py
+@Time    :   2020/09/19 18:52:44
+@Author  :   wylu
+@Version :   1.0
+@Contact :   15wylu@gmail.com
+@License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+@Desc    :
+"""
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        ans, stack = [], []
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            ans.append(root.val)
+            root = root.right
+
+        return ans
