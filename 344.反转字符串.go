b651c3a1d5b5568183227beb96985f9e8d979b50
wylu <158677478@qq.com>
2020-10-02 16:24:38 +0800

    反转字符串

---
+++ 344.反转字符串.go
@@ -0,0 +1,63 @@
+package leetcodecn
+
+/*
+ * @lc app=leetcode.cn id=344 lang=golang
+ *
+ * [344] 反转字符串
+ *
+ * https://leetcode-cn.com/problems/reverse-string/description/
+ *
+ * algorithms
+ * Easy (71.99%)
+ * Likes:    285
+ * Dislikes: 0
+ * Total Accepted:    187.6K
+ * Total Submissions: 260.6K
+ * Testcase Example:  '["h","e","l","l","o"]'
+ *
+ * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+ *
+ * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+ *
+ * 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：["h","e","l","l","o"]
+ * 输出：["o","l","l","e","h"]
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：["H","a","n","n","a","h"]
+ * 输出：["h","a","n","n","a","H"]
+ *
+ */
+
+/**
+ * @File    :   344.反转字符串.go
+ * @Time    :   2020/10/02 16:23:12
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+// @lc code=start
+func reverseString(s []byte) {
+	if len(s) == 0 {
+		return
+	}
+
+	i, j := 0, len(s)-1
+	for i < j {
+		s[i], s[j] = s[j], s[i]
+		i++
+		j--
+	}
+}
+
+// @lc code=end
---
+++ src/python/explore/string/exercise/反转字符串.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   反转字符串.py
+@Time    :   2020/10/02 16:20:42
+@Author  :   wylu
+@Version :   1.0
+@Contact :   15wylu@gmail.com
+@License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+@Desc    :
+"""
+from typing import List
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        if not s:
+            return
+
+        i, j = 0, len(s) - 1
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
