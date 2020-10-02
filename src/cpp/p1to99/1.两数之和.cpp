54ecc0584072ee06c4d7cefb46a6b77a6cdd08f4
wylu <158677478@qq.com>
2020-10-03 00:22:02 +0800

    两数之和

---
+++ src/cpp/p1to99/1.两数之和.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ *
+ * https://leetcode-cn.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (49.45%)
+ * Likes:    9261
+ * Dislikes: 0
+ * Total Accepted:    1.4M
+ * Total Submissions: 2.9M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ * 
+ * 
+ * 
+ * 示例:
+ * 
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ * 
+ * 
+ */
+
+/**
+ * @File    :   1.两数之和.cpp
+ * @Time    :   2020/10/03 00:14:07
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> indices;
+        for (int i = 0; i < nums.size(); i++) {
+            auto it = indices.find(target - nums[i]);
+            if (it != indices.end()) {
+                return {it->second, i};
+            }
+            indices[nums[i]] = i;
+        }
+        return {};
+    }
+};
+// @lc code=end
