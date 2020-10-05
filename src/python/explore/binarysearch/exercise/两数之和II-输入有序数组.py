145013cd2cd11cdcacdbdda1b5a96a1ae34bc114
wylu <158677478@qq.com>
2020-10-05 11:24:17 +0800

    两数之和II-输入有序数组

---
+++ src/python/explore/binarysearch/exercise/两数之和II-输入有序数组.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   两数之和II-输入有序数组.py
+@Time    :   2020/10/05 11:21:54
+@Author  :   wylu
+@Version :   1.0
+@Contact :   15wylu@gmail.com
+@License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+@Desc    :
+"""
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        i, j = 0, len(nums) - 1
+        while i < j:
+            cur = nums[i] + nums[j]
+            if cur == target:
+                return i + 1, j + 1
+            if cur < target:
+                i += 1
+            else:
+                j -= 1
+        return [-1, -1]
