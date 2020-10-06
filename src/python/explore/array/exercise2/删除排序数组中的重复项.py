7183ee8be25a8387b2c19984974f0d7c9e687046
wylu <158677478@qq.com>
2020-10-06 18:37:19 +0800

    删除排序数组中的重复项

---
+++ src/python/explore/array/exercise2/删除排序数组中的重复项.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   删除排序数组中的重复项.py
+@Time    :   2020/10/06 18:35:55
+@Author  :   wylu
+@Version :   1.0
+@Contact :   15wylu@gmail.com
+@License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+@Desc    :
+"""
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[j]:
+                j += 1
+                nums[j] = nums[i]
+        return j + 1
