97a2a8c4151f357440cdf2658774482299b5012b
wylu <158677478@qq.com>
2020-10-06 18:35:32 +0800

    移除元素

---
+++ src/python/explore/array/exercise2/移除元素.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   移除元素.py
+@Time    :   2020/10/06 18:34:50
+@Author  :   wylu
+@Version :   1.0
+@Contact :   15wylu@gmail.com
+@License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+@Desc    :
+"""
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+        return j
