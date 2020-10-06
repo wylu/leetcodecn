3d9f8a90321cf24aa07cd65b9df4acec34848d93
wylu <158677478@qq.com>
2020-10-06 18:33:43 +0800

    移动零

---
+++ src/python/explore/array/exercise2/移动零.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   移动零.py
+@Time    :   2020/10/06 18:32:40
+@Author  :   wylu
+@Version :   1.0
+@Contact :   15wylu@gmail.com
+@License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+@Desc    :
+"""
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i, j, n = 0, 0, len(nums)
+        while j < n:
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+            j += 1
