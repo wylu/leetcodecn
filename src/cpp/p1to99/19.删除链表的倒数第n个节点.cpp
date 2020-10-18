8fb37e1ba95730b64d3ceb85efa409cf2d0ad3b1
wylu <158677478@qq.com>
2020-10-18 22:04:10 +0800

    删除链表的倒数第n个节点

---
+++ src/cpp/p1to99/19.删除链表的倒数第n个节点.cpp
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=cpp
+ *
+ * [19] 删除链表的倒数第N个节点
+ *
+ * https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/description/
+ *
+ * algorithms
+ * Medium (39.84%)
+ * Likes:    1069
+ * Dislikes: 0
+ * Total Accepted:    265.7K
+ * Total Submissions: 660.2K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ * 
+ * 示例：
+ * 
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ * 
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ * 
+ * 
+ * 说明：
+ * 
+ * 给定的 n 保证是有效的。
+ * 
+ * 进阶：
+ * 
+ * 你能尝试使用一趟扫描实现吗？
+ * 
+ */
+
+/**
+ * @File    :   19.删除链表的倒数第n个节点.cpp
+ * @Time    :   2020/10/18 22:00:15
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// @lc code=start
+class Solution {
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n) {
+        ListNode *dummy = new ListNode();
+        dummy->next = head;
+        ListNode *cur = dummy;
+
+        while (n--) head = head->next;
+        while (head) {
+            head = head->next;
+            cur = cur->next;
+        }
+
+        cur->next = cur->next->next;
+        return dummy->next;
+    }
+};
+// @lc code=end
---
+++ src/golang/p1to99/19.删除链表的倒数第n个节点.go
@@ -0,0 +1,63 @@
+package p1to99
+
+/*
+ * @lc app=leetcode.cn id=19 lang=golang
+ *
+ * [19] 删除链表的倒数第N个节点
+ *
+ * https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/description/
+ *
+ * algorithms
+ * Medium (39.84%)
+ * Likes:    1069
+ * Dislikes: 0
+ * Total Accepted:    265.7K
+ * Total Submissions: 660.2K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ *
+ * 示例：
+ *
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ *
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ *
+ *
+ * 说明：
+ *
+ * 给定的 n 保证是有效的。
+ *
+ * 进阶：
+ *
+ * 你能尝试使用一趟扫描实现吗？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{}
+	dummy.Next = head
+	cur := dummy
+
+	for i := 0; i < n; i++ {
+		head = head.Next
+	}
+
+	for head != nil {
+		head = head.Next
+		cur = cur.Next
+	}
+
+	cur.Next = cur.Next.Next
+	return dummy.Next
+}
+
+// @lc code=end
