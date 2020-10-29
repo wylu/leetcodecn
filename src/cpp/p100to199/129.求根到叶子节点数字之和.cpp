3c6347629eeffea5a033cb763e345befc2830072
wylu <158677478@qq.com>
2020-10-29 09:14:42 +0800

    求根到叶子节点数字之和

---
+++ src/cpp/p100to199/129.求根到叶子节点数字之和.cpp
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode.cn id=129 lang=cpp
+ *
+ * [129] 求根到叶子节点数字之和
+ *
+ * https://leetcode-cn.com/problems/sum-root-to-leaf-numbers/description/
+ *
+ * algorithms
+ * Medium (65.59%)
+ * Likes:    222
+ * Dislikes: 0
+ * Total Accepted:    46.4K
+ * Total Submissions: 70.8K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个二叉树，它的每个结点都存放一个 0-9 的数字，每条从根到叶子节点的路径都代表一个数字。
+ * 
+ * 例如，从根到叶子节点路径 1->2->3 代表数字 123。
+ * 
+ * 计算从根到叶子节点生成的所有数字之和。
+ * 
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [1,2,3]
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   3
+ * 输出: 25
+ * 解释:
+ * 从根到叶子节点路径 1->2 代表数字 12.
+ * 从根到叶子节点路径 1->3 代表数字 13.
+ * 因此，数字总和 = 12 + 13 = 25.
+ * 
+ * 示例 2:
+ * 
+ * 输入: [4,9,0,5,1]
+ * ⁠   4
+ * ⁠  / \
+ * ⁠ 9   0
+ * / \
+ * 5   1
+ * 输出: 1026
+ * 解释:
+ * 从根到叶子节点路径 4->9->5 代表数字 495.
+ * 从根到叶子节点路径 4->9->1 代表数字 491.
+ * 从根到叶子节点路径 4->0 代表数字 40.
+ * 因此，数字总和 = 495 + 491 + 40 = 1026.
+ * 
+ */
+
+/**
+ * @File    :   129.求根到叶子节点数字之和.cpp
+ * @Time    :   2020/10/29 09:08:06
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+// @lc code=start
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) { return dfs(root, 0); }
+
+    int dfs(TreeNode* root, int cur) {
+        if (!root) return 0;
+        cur = cur * 10 + root->val;
+        if (!root->left && !root->right) return cur;
+        return dfs(root->left, cur) + dfs(root->right, cur);
+    }
+};
+// @lc code=end
