3d502882f845982cc2785b463166dc205a8c63d8
wylu <158677478@qq.com>
2020-11-01 22:44:36 +0800

    单词拆分-ii

---
+++ src/cpp/p100to199/140.单词拆分-ii.cpp
@@ -0,0 +1,107 @@
+/*
+ * @lc app=leetcode.cn id=140 lang=cpp
+ *
+ * [140] 单词拆分 II
+ *
+ * https://leetcode-cn.com/problems/word-break-ii/description/
+ *
+ * algorithms
+ * Hard (42.44%)
+ * Likes:    331
+ * Dislikes: 0
+ * Total Accepted:    31.6K
+ * Total Submissions: 74.6K
+ * Testcase Example:  '"catsanddog"\n["cat","cats","and","sand","dog"]'
+ *
+ * 给定一个非空字符串 s 和一个包含非空单词列表的字典
+ * wordDict，在字符串中增加空格来构建一个句子，使得句子中所有的单词都在词典中。返回所有这些可能的句子。
+ * 
+ * 说明：
+ * 
+ * 
+ * 分隔时可以重复使用字典中的单词。
+ * 你可以假设字典中没有重复的单词。
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入:
+ * s = "catsanddog"
+ * wordDict = ["cat", "cats", "and", "sand", "dog"]
+ * 输出:
+ * [
+ * "cats and dog",
+ * "cat sand dog"
+ * ]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入:
+ * s = "pineapplepenapple"
+ * wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+ * 输出:
+ * [
+ * "pine apple pen apple",
+ * "pineapple pen apple",
+ * "pine applepen apple"
+ * ]
+ * 解释: 注意你可以重复使用字典中的单词。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入:
+ * s = "catsandog"
+ * wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出:
+ * []
+ * 
+ * 
+ */
+
+/**
+ * @File    :   140.单词拆分-ii.cpp
+ * @Time    :   2020/11/01 22:36:00
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+    unordered_map<int, vector<string>> ans;
+    unordered_set<string> wordSet;
+
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        wordSet = unordered_set(wordDict.begin(), wordDict.end());
+        backtrack(s, 0);
+        return ans[0];
+    }
+
+    void backtrack(const string& s, int cur) {
+        if (ans.count(cur)) return;
+        if (cur == s.length()) {
+            ans[cur] = {""};
+            return;
+        }
+
+        ans[cur] = {};
+        for (int i = cur + 1; i <= s.length(); i++) {
+            string word = s.substr(cur, i - cur);
+            if (wordSet.count(word)) {
+                backtrack(s, i);
+                for (const string& stc : ans[i]) {
+                    ans[cur].push_back(stc.empty() ? word : word + " " + stc);
+                }
+            }
+        }
+    }
+};
+// @lc code=end
