2c723bc8fb0f3bf1c0c6a786c61a454f23dfc88b
wylu <158677478@qq.com>
2020-11-25 13:41:26 +0800

    上升下降字符串

---
+++ src/python/p1300to1399/1370.上升下降字符串.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   1370.上升下降字符串.py
+@Time    :   2020/11/25 13:19:07
+@Author  :   wylu
+@Version :   1.0
+@Contact :   15wylu@gmail.com
+@License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+@Desc    :
+"""
+
+#
+# @lc app=leetcode.cn id=1370 lang=python3
+#
+# [1370] 上升下降字符串
+#
+# https://leetcode-cn.com/problems/increasing-decreasing-string/description/
+#
+# algorithms
+# Easy (74.22%)
+# Likes:    50
+# Dislikes: 0
+# Total Accepted:    24K
+# Total Submissions: 30.5K
+# Testcase Example:  '"aaaabbbbcccc"'
+#
+# 给你一个字符串 s ，请你根据下面的算法重新构造字符串：
+#
+#
+# 从 s 中选出 最小 的字符，将它 接在 结果字符串的后面。
+# 从 s 剩余字符中选出 最小 的字符，且该字符比上一个添加的字符大，将它 接在 结果字符串后面。
+# 重复步骤 2 ，直到你没法从 s 中选择字符。
+# 从 s 中选出 最大 的字符，将它 接在 结果字符串的后面。
+# 从 s 剩余字符中选出 最大 的字符，且该字符比上一个添加的字符小，将它 接在 结果字符串后面。
+# 重复步骤 5 ，直到你没法从 s 中选择字符。
+# 重复步骤 1 到 6 ，直到 s 中所有字符都已经被选过。
+#
+#
+# 在任何一步中，如果最小或者最大字符不止一个 ，你可以选择其中任意一个，并将其添加到结果字符串。
+#
+# 请你返回将 s 中字符重新排序后的 结果字符串 。
+#
+#
+#
+# 示例 1：
+#
+# 输入：s = "aaaabbbbcccc"
+# 输出："abccbaabccba"
+# 解释：第一轮的步骤 1，2，3 后，结果字符串为 result = "abc"
+# 第一轮的步骤 4，5，6 后，结果字符串为 result = "abccba"
+# 第一轮结束，现在 s = "aabbcc" ，我们再次回到步骤 1
+# 第二轮的步骤 1，2，3 后，结果字符串为 result = "abccbaabc"
+# 第二轮的步骤 4，5，6 后，结果字符串为 result = "abccbaabccba"
+#
+#
+# 示例 2：
+#
+# 输入：s = "rat"
+# 输出："art"
+# 解释：单词 "rat" 在上述算法重排序以后变成 "art"
+#
+#
+# 示例 3：
+#
+# 输入：s = "leetcode"
+# 输出："cdelotee"
+#
+#
+# 示例 4：
+#
+# 输入：s = "ggggggg"
+# 输出："ggggggg"
+#
+#
+# 示例 5：
+#
+# 输入：s = "spo"
+# 输出："ops"
+#
+#
+#
+#
+# 提示：
+#
+#
+# 1 <= s.length <= 500
+# s 只包含小写英文字母。
+#
+#
+#
+
+
+# @lc code=start
+class Solution:
+    def sortString(self, s: str) -> str:
+        cnts = [0] * 26
+        for ch in s:
+            cnts[ord(ch) - ord('a')] += 1
+
+        def addChar(i: int) -> None:
+            if cnts[i] > 0:
+                ans.append(chr(i + ord('a')))
+                cnts[i] -= 1
+
+        ans = []
+        while len(ans) < len(s):
+            for i in range(26):
+                addChar(i)
+            for i in range(25, -1, -1):
+                addChar(i)
+
+        return ''.join(ans)
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    solu = Solution()
+    print(solu.sortString("aaaabbbbcccc"))
+    print(solu.sortString("rat"))
+    print(solu.sortString("leetcode"))
+    print(solu.sortString("ggggggg"))
+    print(solu.sortString("spo"))
