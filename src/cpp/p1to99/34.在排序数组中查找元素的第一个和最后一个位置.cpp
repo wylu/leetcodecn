afddeb988204606ef5bedc6a740aeb6de7d771df
wylu <158677478@qq.com>
2020-12-01 20:35:14 +0800

    在排序数组中查找元素的第一个和最后一个位置

---
+++ src/cpp/p1to99/34.在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode.cn id=34 lang=cpp
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ *
+ * https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ *
+ * algorithms
+ * Medium (41.43%)
+ * Likes:    725
+ * Dislikes: 0
+ * Total Accepted:    170.6K
+ * Total Submissions: 411K
+ * Testcase Example:  '[5,7,7,8,8,10]\n8'
+ *
+ * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+ * 
+ * 如果数组中不存在目标值 target，返回 [-1, -1]。
+ * 
+ * 进阶：
+ * 
+ * 
+ * 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [5,7,7,8,8,10], target = 8
+ * 输出：[3,4]
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [5,7,7,8,8,10], target = 6
+ * 输出：[-1,-1]
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [], target = 0
+ * 输出：[-1,-1]
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 <= nums.length <= 10^5
+ * -10^9 <= nums[i] <= 10^9
+ * nums 是一个非递减数组
+ * -10^9 <= target <= 10^9
+ * 
+ * 
+ */
+
+/**
+ * @File    :   34.在排序数组中查找元素的第一个和最后一个位置.cpp
+ * @Time    :   2020/12/01 16:13:17
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int start = search(nums, target);
+        if (start == nums.size() || nums[start] != target) return {-1, -1};
+        int end = search(nums, target + 1);
+        return {start, end - 1};
+    }
+
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size();
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+};
+// @lc code=end
