622e94253f3799c52764fd6b2ea9842f5f7d44b1
wylu <158677478@qq.com>
2020-12-09 14:55:54 +0800

    不同路径-ii

---
+++ src/python/p1to99/63.不同路径-ii.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   63.不同路径-ii.py
+@Time    :   2020/12/09 14:39:03
+@Author  :   wylu
+@Version :   1.0
+@Contact :   15wylu@gmail.com
+@License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+@Desc    :
+"""
+
+#
+# @lc app=leetcode.cn id=63 lang=python3
+#
+# [63] 不同路径 II
+#
+# https://leetcode-cn.com/problems/unique-paths-ii/description/
+#
+# algorithms
+# Medium (37.17%)
+# Likes:    454
+# Dislikes: 0
+# Total Accepted:    112.7K
+# Total Submissions: 303.3K
+# Testcase Example:  '[[0,0,0],[0,1,0],[0,0,0]]'
+#
+# 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+#
+# 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+#
+# 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+#
+#
+#
+# 网格中的障碍物和空位置分别用 1 和 0 来表示。
+#
+#
+#
+# 示例 1：
+#
+#
+# 输入：obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+# 输出：2
+# 解释：
+# 3x3 网格的正中间有一个障碍物。
+# 从左上角到右下角一共有 2 条不同的路径：
+# 1. 向右 -> 向右 -> 向下 -> 向下
+# 2. 向下 -> 向下 -> 向右 -> 向右
+#
+#
+# 示例 2：
+#
+#
+# 输入：obstacleGrid = [[0,1],[0,0]]
+# 输出：1
+#
+#
+#
+#
+# 提示：
+#
+#
+# m == obstacleGrid.length
+# n == obstacleGrid[i].length
+# 1 <= m, n <= 100
+# obstacleGrid[i][j] 为 0 或 1
+#
+#
+#
+from typing import List
+
+
+# @lc code=start
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [0] * n
+
+        dp[0] = 1 - obstacleGrid[0][0]
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    dp[j] = 0
+                elif j > 0:
+                    dp[j] += dp[j - 1]
+
+        return dp[n - 1]
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    solu = Solution()
+    grid = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+    print(solu.uniquePathsWithObstacles(grid))
+
+    grid = [[0, 1], [0, 0]]
+    print(solu.uniquePathsWithObstacles(grid))
+
+    grid = [[1]]
+    print(solu.uniquePathsWithObstacles(grid))
+
+    grid = [[0], [1]]
+    print(solu.uniquePathsWithObstacles(grid))
+
+    grid = [[0, 0], [1, 0]]
+    print(solu.uniquePathsWithObstacles(grid))
