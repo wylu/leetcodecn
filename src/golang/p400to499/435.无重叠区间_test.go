f6660ed32014c3aab0784cbd9c860f658f37438f
wylu <158677478@qq.com>
2020-12-31 21:45:30 +0800

    无重叠区间

---
+++ src/golang/p400to499/435.无重叠区间_test.go
@@ -0,0 +1,46 @@
+package p400to499
+
+import "testing"
+
+func Test_eraseOverlapIntervals(t *testing.T) {
+	type args struct {
+		intervals [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// Case 1
+		{
+			name: "Case 1",
+			args: args{
+				intervals: [][]int{{1, 2}, {2, 3}, {3, 4}, {1, 3}},
+			},
+			want: 1,
+		},
+		// Case 2
+		{
+			name: "Case 2",
+			args: args{
+				intervals: [][]int{{1, 2}, {1, 2}, {1, 2}},
+			},
+			want: 2,
+		},
+		// Case 3
+		{
+			name: "Case 3",
+			args: args{
+				intervals: [][]int{{1, 2}, {2, 3}},
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := eraseOverlapIntervals(tt.args.intervals); got != tt.want {
+				t.Errorf("eraseOverlapIntervals() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
