b58746b6c79a425ea414876fac3153e272238660
wylu <158677478@qq.com>
2021-01-20 19:11:15 +0800

    三个数的最大乘积

---
+++ src/cpp/p600to699/628.三个数的最大乘积.cpp
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=628 lang=cpp
+ *
+ * [628] 三个数的最大乘积
+ *
+ * https://leetcode-cn.com/problems/maximum-product-of-three-numbers/description/
+ *
+ * algorithms
+ * Easy (51.73%)
+ * Likes:    254
+ * Dislikes: 0
+ * Total Accepted:    56.2K
+ * Total Submissions: 108.5K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个整型数组，在数组中找出由三个数组成的最大乘积，并输出这个乘积。
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: [1,2,3]
+ * 输出: 6
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: [1,2,3,4]
+ * 输出: 24
+ * 
+ * 
+ * 注意:
+ * 
+ * 
+ * 给定的整型数组长度范围是[3,10^4]，数组中所有的元素范围是[-1000, 1000]。
+ * 输入的数组中任意三个数的乘积不会超出32位有符号整数的范围。
+ * 
+ * 
+ */
+
+/**
+ * @File    :   628.三个数的最大乘积.cpp
+ * @Time    :   2021/01/20 19:07:14
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        return max(nums[0] * nums[1] * nums[n - 1],
+                   max(nums[0] * nums[1] * nums[2],
+                       nums[n - 3] * nums[n - 2] * nums[n - 1]));
+    }
+};
+// @lc code=end
