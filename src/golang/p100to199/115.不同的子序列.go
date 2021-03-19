47c7c555d7389bcad5c1a27c0bc89675dd6773e5
wylu <158677478@qq.com>
2021-03-19 13:29:30 +0800

    不同的子序列

---
+++ src/golang/p100to199/115.不同的子序列.go
@@ -0,0 +1,108 @@
+package p100to199
+
+/*
+ * @lc app=leetcode.cn id=115 lang=golang
+ *
+ * [115] 不同的子序列
+ *
+ * https://leetcode-cn.com/problems/distinct-subsequences/description/
+ *
+ * algorithms
+ * Hard (54.21%)
+ * Likes:    479
+ * Dislikes: 0
+ * Total Accepted:    44.8K
+ * Total Submissions: 82.6K
+ * Testcase Example:  '"rabbbit"\n"rabbit"'
+ *
+ * 给定一个字符串 s 和一个字符串 t ，计算在 s 的子序列中 t 出现的个数。
+ *
+ * 字符串的一个 子序列 是指，通过删除一些（也可以不删除）字符且不干扰剩余字符相对位置所组成的新字符串。（例如，"ACE" 是 "ABCDE"
+ * 的一个子序列，而 "AEC" 不是）
+ *
+ * 题目数据保证答案符合 32 位带符号整数范围。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：s = "rabbbit", t = "rabbit"
+ * 输出：3
+ * 解释：
+ * 如下图所示, 有 3 种可以从 s 中得到 "rabbit" 的方案。
+ * (上箭头符号 ^ 表示选取的字母)
+ * rabbbit
+ * ^^^^ ^^
+ * rabbbit
+ * ^^ ^^^^
+ * rabbbit
+ * ^^^ ^^^
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：s = "babgbag", t = "bag"
+ * 输出：5
+ * 解释：
+ * 如下图所示, 有 5 种可以从 s 中得到 "bag" 的方案。
+ * (上箭头符号 ^ 表示选取的字母)
+ * babgbag
+ * ^^ ^
+ * babgbag
+ * ^^    ^
+ * babgbag
+ * ^    ^^
+ * babgbag
+ * ⁠ ^  ^^
+ * babgbag
+ * ⁠   ^^^
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 0 <= s.length, t.length <= 1000
+ * s 和 t 由英文字母组成
+ *
+ *
+ */
+
+/**
+ * @File    :   115.不同的子序列.go
+ * @Time    :   2021/03/19 13:23:56
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+// @lc code=start
+func numDistinct(s string, t string) int {
+	m, n := len(t)+1, len(s)+1
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+
+	for i := 0; i < n; i++ {
+		dp[0][i] = 1
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if t[i-1] == s[j-1] {
+				dp[i][j] = dp[i-1][j-1] + dp[i][j-1]
+			} else {
+				dp[i][j] = dp[i][j-1]
+			}
+		}
+	}
+
+	return dp[m-1][n-1]
+}
+
+// @lc code=end
