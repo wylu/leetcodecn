1147d63beccc1667e6d8661d85483e1fe20c742d
wylu <158677478@qq.com>
2021-03-26 22:29:11 +0800

    删除排序链表中的重复元素

---
+++ src/cpp/p1to99/83.删除排序链表中的重复元素.cpp
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=cpp
+ *
+ * [83] 删除排序链表中的重复元素
+ *
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/description/
+ *
+ * algorithms
+ * Easy (53.55%)
+ * Likes:    537
+ * Dislikes: 0
+ * Total Accepted:    231.6K
+ * Total Submissions: 432.4K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+ * 
+ * 返回同样按升序排列的结果链表。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,1,2]
+ * 输出：[1,2]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = [1,1,2,3,3]
+ * 输出：[1,2,3]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中节点数目在范围 [0, 300] 内
+ * -100 <= Node.val <= 100
+ * 题目数据保证链表已经按升序排列
+ * 
+ * 
+ */
+
+/**
+ * @File    :   83.删除排序链表中的重复元素.cpp
+ * @Time    :   2021/03/26 22:18:30
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// @lc code=start
+class Solution {
+public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        ListNode *dummy = new ListNode(-200);
+        ListNode *pre = dummy, *cur = head;
+        while (cur) {
+            if (cur->val != pre->val) {
+                pre->next = cur;
+                pre = pre->next;
+            }
+            cur = cur->next;
+        }
+        pre->next = nullptr;
+        return dummy->next;
+    }
+};
+// @lc code=end
---
+++ src/golang/p1to99/83.删除排序链表中的重复元素.go
@@ -0,0 +1,82 @@
+package p1to99
+
+/*
+ * @lc app=leetcode.cn id=83 lang=golang
+ *
+ * [83] 删除排序链表中的重复元素
+ *
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/description/
+ *
+ * algorithms
+ * Easy (53.55%)
+ * Likes:    537
+ * Dislikes: 0
+ * Total Accepted:    231.6K
+ * Total Submissions: 432.4K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+ *
+ * 返回同样按升序排列的结果链表。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,1,2]
+ * 输出：[1,2]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：head = [1,1,2,3,3]
+ * 输出：[1,2,3]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 链表中节点数目在范围 [0, 300] 内
+ * -100 <= Node.val <= 100
+ * 题目数据保证链表已经按升序排列
+ *
+ *
+ */
+
+/**
+ * @File    :   83.删除排序链表中的重复元素.go
+ * @Time    :   2021/03/26 22:25:25
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	dummy := &ListNode{Val: -200}
+	pre, cur := dummy, head
+	for cur != nil {
+		if cur.Val != pre.Val {
+			pre.Next = cur
+			pre = cur
+		}
+		cur = cur.Next
+	}
+	pre.Next = nil
+	return dummy.Next
+}
+
+// @lc code=end
