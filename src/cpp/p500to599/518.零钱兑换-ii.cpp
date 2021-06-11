37ef588bfde586dfc3380e33fe04417cb9a97f45
wylu <158677478@qq.com>
2021-06-11 09:03:45 +0800

    零钱兑换-ii

---
+++ src/cpp/p500to599/518.零钱兑换-ii.cpp
@@ -0,0 +1,99 @@
+/*
+ * @lc app=leetcode.cn id=518 lang=cpp
+ *
+ * [518] 零钱兑换 II
+ *
+ * https://leetcode-cn.com/problems/coin-change-2/description/
+ *
+ * algorithms
+ * Medium (63.36%)
+ * Likes:    523
+ * Dislikes: 0
+ * Total Accepted:    80.6K
+ * Total Submissions: 127.2K
+ * Testcase Example:  '5\n[1,2,5]'
+ *
+ * 给你一个整数数组 coins 表示不同面额的硬币，另给一个整数 amount 表示总金额。
+ * 
+ * 请你计算并返回可以凑成总金额的硬币组合数。如果任何硬币组合都无法凑出总金额，返回 0 。
+ * 
+ * 假设每一种面额的硬币有无限个。 
+ * 
+ * 题目数据保证结果符合 32 位带符号整数。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：amount = 5, coins = [1, 2, 5]
+ * 输出：4
+ * 解释：有四种方式可以凑成总金额：
+ * 5=5
+ * 5=2+2+1
+ * 5=2+1+1+1
+ * 5=1+1+1+1+1
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：amount = 3, coins = [2]
+ * 输出：0
+ * 解释：只用面额 2 的硬币不能凑成总金额 3 。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：amount = 10, coins = [10] 
+ * 输出：1
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= coins.length <= 300
+ * 1 <= coins[i] <= 5000
+ * coins 中的所有值 互不相同
+ * 0 <= amount <= 5000
+ * 
+ * 
+ */
+
+/**
+ * @File    :   518.零钱兑换-ii.cpp
+ * @Time    :   2021/06/11 09:01:43
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<int> f(amount + 1);
+        f[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = coins[i - 1]; j <= amount; j++) {
+                f[j] += f[j - coins[i - 1]];
+            }
+        }
+
+        return f[amount];
+    }
+};
+// @lc code=end
