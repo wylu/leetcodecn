75a42e158e3aac380116cca396b1f476859f31d3
wylu <158677478@qq.com>
2021-06-28 09:51:10 +0800

    蛇梯棋

---
+++ src/cpp/p900to999/909.蛇梯棋.cpp
@@ -0,0 +1,149 @@
+/*
+ * @lc app=leetcode.cn id=909 lang=cpp
+ *
+ * [909] 蛇梯棋
+ *
+ * https://leetcode-cn.com/problems/snakes-and-ladders/description/
+ *
+ * algorithms
+ * Medium (46.69%)
+ * Likes:    74
+ * Dislikes: 0
+ * Total Accepted:    14.1K
+ * Total Submissions: 30.2K
+ * Testcase Example:  '[[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,35,-1,-1,13,-1],[-1,-1,-1,-1,-1,-1],[-1,15,-1,-1,-1,-1]]'
+ *
+ * N x N 的棋盘 board 上，按从 1 到 N*N 的数字给方格编号，编号 从左下角开始，每一行交替方向。
+ * 
+ * 例如，一块 6 x 6 大小的棋盘，编号如下：
+ * 
+ * 
+ * 
+ * 
+ * r 行 c 列的棋盘，按前述方法编号，棋盘格中可能存在 “蛇” 或 “梯子”；如果 board[r][c] != -1，那个蛇或梯子的目的地将会是
+ * board[r][c]。
+ * 
+ * 玩家从棋盘上的方格 1 （总是在最后一行、第一列）开始出发。
+ * 
+ * 每一回合，玩家需要从当前方格 x 开始出发，按下述要求前进：
+ * 
+ * 
+ * 选定目标方格：选择从编号 x+1，x+2，x+3，x+4，x+5，或者 x+6 的方格中选出一个目标方格 s ，目标方格的编号 <=
+ * N*N。
+ * 
+ * 
+ * 该选择模拟了掷骰子的情景，无论棋盘大小如何，你的目的地范围也只能处于区间 [x+1, x+6] 之间。
+ * 
+ * 
+ * 传送玩家：如果目标方格 S 处存在蛇或梯子，那么玩家会传送到蛇或梯子的目的地。否则，玩家传送到目标方格 S。 
+ * 
+ * 
+ * 注意，玩家在每回合的前进过程中最多只能爬过蛇或梯子一次：就算目的地是另一条蛇或梯子的起点，你也不会继续移动。
+ * 
+ * 返回达到方格 N*N 所需的最少移动次数，如果不可能，则返回 -1。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 输入：[
+ * [-1,-1,-1,-1,-1,-1],
+ * [-1,-1,-1,-1,-1,-1],
+ * [-1,-1,-1,-1,-1,-1],
+ * [-1,35,-1,-1,13,-1],
+ * [-1,-1,-1,-1,-1,-1],
+ * [-1,15,-1,-1,-1,-1]]
+ * 输出：4
+ * 解释：
+ * 首先，从方格 1 [第 5 行，第 0 列] 开始。
+ * 你决定移动到方格 2，并必须爬过梯子移动到到方格 15。
+ * 然后你决定移动到方格 17 [第 3 行，第 5 列]，必须爬过蛇到方格 13。
+ * 然后你决定移动到方格 14，且必须通过梯子移动到方格 35。
+ * 然后你决定移动到方格 36, 游戏结束。
+ * 可以证明你需要至少 4 次移动才能到达第 N*N 个方格，所以答案是 4。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 2 <= board.length = board[0].length <= 20
+ * board[i][j] 介于 1 和 N*N 之间或者等于 -1。
+ * 编号为 1 的方格上没有蛇或梯子。
+ * 编号为 N*N 的方格上没有蛇或梯子。
+ * 
+ * 
+ */
+
+/**
+ * @File    :   909.蛇梯棋.cpp
+ * @Time    :   2021/06/28 09:10:28
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+#define PII pair<int, int>
+
+class Solution {
+    int n = 0;
+    PII getPosition(int num) {
+        int r = num / n, c = num % n;
+        if (r % 2 != 0) c = n - c - 1;
+        r = n - r - 1;
+        return {r, c};
+    }
+
+public:
+    int snakesAndLadders(vector<vector<int>>& board) {
+        n = board.size();
+
+        int ans = 0;
+        unordered_set<int> seen = {1};
+        queue<int> que;
+        que.push(1);
+        while (!que.empty()) {
+            ans++;
+            int size = que.size();
+            for (int i = 0; i < size; i++) {
+                int num = que.front();
+                que.pop();
+
+                for (int i = 1; i <= 6; i++) {
+                    int next_num = num + i;
+                    if (next_num > n * n) break;
+
+                    PII it = getPosition(next_num - 1);
+                    int r = it.first, c = it.second;
+                    if (board[r][c] != -1) next_num = board[r][c];
+                    if (next_num == n * n) return ans;
+                    // cout << r << " " << c << " " << next_num << endl;
+                    if (!seen.count(next_num)) {
+                        seen.insert(next_num);
+                        que.push(next_num);
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+};
+// @lc code=end
+
+int main(int argc, char const* argv[]) {
+    Solution solu;
+    vector<vector<int>> board = {
+        {-1, -1, -1, -1, -1, -1}, {-1, -1, -1, -1, -1, -1},
+        {-1, -1, -1, -1, -1, -1}, {-1, 35, -1, -1, 13, -1},
+        {-1, -1, -1, -1, -1, -1}, {-1, 15, -1, -1, -1, -1}};
+    cout << solu.snakesAndLadders(board) << endl;
+    return 0;
+}
