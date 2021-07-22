97559089a181ae4129415e8851b954b342d6f23c
wylu <158677478@qq.com>
2021-07-22 10:16:38 +0800

    复制带随机指针的链表

---
+++ src/golang/p100to199/138.复制带随机指针的链表.go
@@ -0,0 +1,127 @@
+package p100to199
+
+/*
+ * @lc app=leetcode.cn id=138 lang=golang
+ *
+ * [138] 复制带随机指针的链表
+ *
+ * https://leetcode-cn.com/problems/copy-list-with-random-pointer/description/
+ *
+ * algorithms
+ * Medium (62.86%)
+ * Likes:    618
+ * Dislikes: 0
+ * Total Accepted:    83.1K
+ * Total Submissions: 132.3K
+ * Testcase Example:  '[[7,null],[13,0],[11,4],[10,2],[1,0]]'
+ *
+ * 给你一个长度为 n 的链表，每个节点包含一个额外增加的随机指针 random ，该指针可以指向链表中的任何节点或空节点。
+ *
+ * 构造这个链表的 深拷贝。 深拷贝应该正好由 n 个 全新 节点组成，其中每个新节点的值都设为其对应的原节点的值。新节点的 next 指针和 random
+ * 指针也都应指向复制链表中的新节点，并使原链表和复制链表中的这些指针能够表示相同的链表状态。复制链表中的指针都不应指向原链表中的节点 。
+ *
+ * 例如，如果原链表中有 X 和 Y 两个节点，其中 X.random --> Y 。那么在复制链表中对应的两个节点 x 和 y ，同样有 x.random
+ * --> y 。
+ *
+ * 返回复制链表的头节点。
+ *
+ * 用一个由 n 个节点组成的链表来表示输入/输出中的链表。每个节点用一个 [val, random_index] 表示：
+ *
+ *
+ * val：一个表示 Node.val 的整数。
+ * random_index：随机指针指向的节点索引（范围从 0 到 n-1）；如果不指向任何节点，则为  null 。
+ *
+ *
+ * 你的代码 只 接受原链表的头节点 head 作为传入参数。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ *
+ * 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ *
+ *
+ * 输入：head = [[1,1],[2,1]]
+ * 输出：[[1,1],[2,1]]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ *
+ *
+ * 输入：head = [[3,null],[3,0],[3,null]]
+ * 输出：[[3,null],[3,0],[3,null]]
+ *
+ *
+ * 示例 4：
+ *
+ *
+ * 输入：head = []
+ * 输出：[]
+ * 解释：给定的链表为空（空指针），因此返回 null。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 0 <= n <= 1000
+ * -10000 <= Node.val <= 10000
+ * Node.random 为空（null）或指向链表中的节点。
+ *
+ *
+ */
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Next *Node
+ *     Random *Node
+ * }
+ */
+
+// @lc code=start
+func copyRandomList(head *Node) *Node {
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		node := &Node{Val: cur.Val, Next: next}
+		cur.Next = node
+		cur = next
+	}
+
+	cur = head
+	for cur != nil {
+		node := cur.Next
+		if cur.Random != nil {
+			node.Random = cur.Random.Next
+		}
+		cur = node.Next
+	}
+
+	// 拆分链表
+	copyHead := &Node{}
+	ol, nl := head, copyHead
+	for ol != nil {
+		nl.Next = ol.Next
+		nl = nl.Next
+		ol.Next = nl.Next
+		ol = ol.Next
+	}
+
+	return copyHead.Next
+}
+
+// @lc code=end
