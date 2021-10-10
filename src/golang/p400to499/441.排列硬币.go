1e095799a31d9e7e2b79e99404d322ea063c464a
wylu <158677478@qq.com>
2021-10-10 21:26:32 +0800

    排列硬币

---
+++ src/golang/p400to499/441.排列硬币.go
@@ -0,0 +1,72 @@
+package p400to499
+
+import "math"
+
+/*
+ * @lc app=leetcode.cn id=441 lang=golang
+ *
+ * [441] 排列硬币
+ *
+ * https://leetcode-cn.com/problems/arranging-coins/description/
+ *
+ * algorithms
+ * Easy (44.04%)
+ * Likes:    143
+ * Dislikes: 0
+ * Total Accepted:    54.8K
+ * Total Submissions: 124.5K
+ * Testcase Example:  '5'
+ *
+ * 你总共有 n 枚硬币，并计划将它们按阶梯状排列。对于一个由 k 行组成的阶梯，其第 i 行必须正好有 i 枚硬币。阶梯的最后一行 可能 是不完整的。
+ *
+ * 给你一个数字 n ，计算并返回可形成 完整阶梯行 的总行数。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：n = 5
+ * 输出：2
+ * 解释：因为第三行不完整，所以返回 2 。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：n = 8
+ * 输出：3
+ * 解释：因为第四行不完整，所以返回 3 。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= n <= 2^31 - 1
+ *
+ *
+ */
+
+/**
+ * @File    :   441.排列硬币.go
+ * @Time    :   2021/10/10 10:19:26
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+// @lc code=start
+func arrangeCoins(n int) int {
+	x := int(math.Sqrt(float64(n * 2)))
+	if n < x*(x+1)/2 {
+		// n 不能填满第 n 项
+		x--
+	}
+	return x
+}
+
+// @lc code=end
