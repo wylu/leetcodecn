525287ce5cfe2a191da0d0e59f21471474f7b58f
wylu <158677478@qq.com>
2021-10-21 14:14:36 +0800

    加一

---
+++ src/golang/p1to99/66.加一.go
@@ -0,0 +1,94 @@
+package p1to99
+
+/*
+ * @lc app=leetcode.cn id=66 lang=golang
+ *
+ * [66] 加一
+ *
+ * https://leetcode-cn.com/problems/plus-one/description/
+ *
+ * algorithms
+ * Easy (46.19%)
+ * Likes:    804
+ * Dislikes: 0
+ * Total Accepted:    373.1K
+ * Total Submissions: 807.8K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+ *
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+ *
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：digits = [1,2,3]
+ * 输出：[1,2,4]
+ * 解释：输入数组表示数字 123。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：digits = [4,3,2,1]
+ * 输出：[4,3,2,2]
+ * 解释：输入数组表示数字 4321。
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：digits = [0]
+ * 输出：[1]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= digits.length <= 100
+ * 0 <= digits[i] <= 9
+ *
+ *
+ */
+
+/**
+ * @File    :   66.加一.go
+ * @Time    :   2021/10/21 13:04:26
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+// @lc code=start
+func plusOne(digits []int) []int {
+	reverse := func(arr []int) {
+		for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+
+	reverse(digits)
+	carry := 1
+	for i, v := range digits {
+		carry += v
+		digits[i] = carry % 10
+		carry /= 10
+	}
+
+	if carry > 0 {
+		digits = append(digits, carry)
+	}
+	reverse(digits)
+
+	return digits
+}
+
+// @lc code=end
