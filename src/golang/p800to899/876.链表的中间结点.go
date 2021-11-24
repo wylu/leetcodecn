e6c3249ddb7e27609055c27e64bd1a48fcdc0fff
wylu <158677478@qq.com>
2021-11-24 23:30:43 +0800

    链表的中间结点

---
+++ src/golang/p800to899/876.链表的中间结点.go
@@ -0,0 +1,80 @@
+package p800to899
+
+/*
+ * @lc app=leetcode.cn id=876 lang=golang
+ *
+ * [876] 链表的中间结点
+ *
+ * https://leetcode-cn.com/problems/middle-of-the-linked-list/description/
+ *
+ * algorithms
+ * Easy (70.62%)
+ * Likes:    440
+ * Dislikes: 0
+ * Total Accepted:    168.8K
+ * Total Submissions: 239K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 给定一个头结点为 head 的非空单链表，返回链表的中间结点。
+ *
+ * 如果有两个中间结点，则返回第二个中间结点。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：[1,2,3,4,5]
+ * 输出：此列表中的结点 3 (序列化形式：[3,4,5])
+ * 返回的结点值为 3 。 (测评系统对该结点序列化表述是 [3,4,5])。
+ * 注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+ * ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next
+ * = NULL.
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：[1,2,3,4,5,6]
+ * 输出：此列表中的结点 4 (序列化形式：[4,5,6])
+ * 由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 给定链表的结点数介于 1 和 100 之间。
+ *
+ *
+ */
+
+/**
+ * @File    :   876.链表的中间结点.go
+ * @Time    :   2021/11/24 23:28:03
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func middleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
+
+// @lc code=end
