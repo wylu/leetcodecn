a84485697559ec653e19cd92df916b79e9e25967
wylu <158677478@qq.com>
2021-11-27 22:13:19 +0800

    最大二叉树

---
+++ src/golang/p600to699/654.最大二叉树.go
@@ -0,0 +1,106 @@
+package p600to699
+
+/*
+ * @lc app=leetcode.cn id=654 lang=golang
+ *
+ * [654] 最大二叉树
+ *
+ * https://leetcode-cn.com/problems/maximum-binary-tree/description/
+ *
+ * algorithms
+ * Medium (80.98%)
+ * Likes:    346
+ * Dislikes: 0
+ * Total Accepted:    66.1K
+ * Total Submissions: 81.7K
+ * Testcase Example:  '[3,2,1,6,0,5]'
+ *
+ * 给定一个不含重复元素的整数数组 nums 。一个以此数组直接递归构建的 最大二叉树 定义如下：
+ *
+ *
+ * 二叉树的根是数组 nums 中的最大元素。
+ * 左子树是通过数组中 最大值左边部分 递归构造出的最大二叉树。
+ * 右子树是通过数组中 最大值右边部分 递归构造出的最大二叉树。
+ *
+ *
+ * 返回有给定数组 nums 构建的 最大二叉树 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [3,2,1,6,0,5]
+ * 输出：[6,3,5,null,2,0,null,null,1]
+ * 解释：递归调用如下所示：
+ * - [3,2,1,6,0,5] 中的最大值是 6 ，左边部分是 [3,2,1] ，右边部分是 [0,5] 。
+ * ⁠   - [3,2,1] 中的最大值是 3 ，左边部分是 [] ，右边部分是 [2,1] 。
+ * ⁠       - 空数组，无子节点。
+ * ⁠       - [2,1] 中的最大值是 2 ，左边部分是 [] ，右边部分是 [1] 。
+ * ⁠           - 空数组，无子节点。
+ * ⁠           - 只有一个元素，所以子节点是一个值为 1 的节点。
+ * ⁠   - [0,5] 中的最大值是 5 ，左边部分是 [0] ，右边部分是 [] 。
+ * ⁠       - 只有一个元素，所以子节点是一个值为 0 的节点。
+ * ⁠       - 空数组，无子节点。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [3,2,1]
+ * 输出：[3,null,2,null,1]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 1000
+ * 0 <= nums[i] <= 1000
+ * nums 中的所有整数 互不相同
+ *
+ *
+ */
+
+/**
+ * @File    :   654.最大二叉树.go
+ * @Time    :   2021/11/27 22:06:48
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func constructMaximumBinaryTree(nums []int) *TreeNode {
+	var dfs func(int, int) *TreeNode
+	dfs = func(left, right int) *TreeNode {
+		if left > right {
+			return nil
+		}
+		idx := left
+		for i := left + 1; i <= right; i++ {
+			if nums[i] > nums[idx] {
+				idx = i
+			}
+		}
+		root := &TreeNode{Val: nums[idx]}
+		root.Left = dfs(left, idx-1)
+		root.Right = dfs(idx+1, right)
+		return root
+	}
+
+	return dfs(0, len(nums)-1)
+}
+
+// @lc code=end
