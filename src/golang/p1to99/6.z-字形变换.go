81ca045bc2ac3f0fb04d832e24836f3e2f169eec
wylu <158677478@qq.com>
2022-03-01 22:22:47 +0800

    z-字形变换

---
+++ src/golang/p1to99/6.z-字形变换.go
@@ -0,0 +1,104 @@
+package p1to99
+
+import "bytes"
+
+/*
+ * @lc app=leetcode.cn id=6 lang=golang
+ *
+ * [6] Z 字形变换
+ *
+ * https://leetcode-cn.com/problems/zigzag-conversion/description/
+ *
+ * algorithms
+ * Medium (51.78%)
+ * Likes:    1568
+ * Dislikes: 0
+ * Total Accepted:    386.6K
+ * Total Submissions: 746.7K
+ * Testcase Example:  '"PAYPALISHIRING"\n3'
+ *
+ * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+ *
+ * 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+ *
+ *
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ *
+ * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+ *
+ * 请你实现这个将字符串进行指定行数变换的函数：
+ *
+ *
+ * string convert(string s, int numRows);
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：s = "PAYPALISHIRING", numRows = 3
+ * 输出："PAHNAPLSIIGYIR"
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：s = "PAYPALISHIRING", numRows = 4
+ * 输出："PINALSIGYAHRPI"
+ * 解释：
+ * P     I    N
+ * A   L S  I G
+ * Y A   H R
+ * P     I
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：s = "A", numRows = 1
+ * 输出："A"
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= s.length <= 1000
+ * s 由英文字母（小写和大写）、',' 和 '.' 组成
+ * 1 <= numRows <= 1000
+ *
+ *
+ */
+
+/**
+ * @File    :   6.z-字形变换.go
+ * @Time    :   2022/03/01 22:12:44
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+// @lc code=start
+func convert(s string, numRows int) string {
+	if numRows == 1 || numRows >= len(s) {
+		return s
+	}
+
+	mat := make([][]byte, numRows)
+	t, x := numRows*2-2, 0
+	for i, ch := range s {
+		mat[x] = append(mat[x], byte(ch))
+		if i%t < numRows-1 {
+			x++
+		} else {
+			x--
+		}
+	}
+	return string(bytes.Join(mat, nil))
+}
+
+// @lc code=end
