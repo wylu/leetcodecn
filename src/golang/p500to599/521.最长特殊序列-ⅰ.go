f29f6a4a52c2fbe4ae069bfec065c6679b4a502d
wylu <158677478@qq.com>
2022-03-05 11:19:04 +0800

    最长特殊序列-ⅰ

---
+++ src/golang/p500to599/521.最长特殊序列-ⅰ.go
@@ -0,0 +1,86 @@
+package p500to599
+
+/*
+ * @lc app=leetcode.cn id=521 lang=golang
+ *
+ * [521] 最长特殊序列 Ⅰ
+ *
+ * https://leetcode-cn.com/problems/longest-uncommon-subsequence-i/description/
+ *
+ * algorithms
+ * Easy (72.93%)
+ * Likes:    155
+ * Dislikes: 0
+ * Total Accepted:    37.2K
+ * Total Submissions: 51.1K
+ * Testcase Example:  '"aba"\n"cdc"'
+ *
+ * 给你两个字符串 a 和 b，请返回 这两个字符串中 最长的特殊序列  。如果不存在，则返回 -1 。
+ *
+ * 「最长特殊序列」 定义如下：该序列为 某字符串独有的最长子序列（即不能是其他字符串的子序列） 。
+ *
+ * 字符串 s 的子序列是在从 s 中删除任意数量的字符后可以获得的字符串。
+ *
+ *
+ * 例如，"abc" 是 "aebdc" 的子序列，因为删除 "aebdc" 中斜体加粗的字符可以得到 "abc" 。 "aebdc" 的子序列还包括
+ * "aebdc" 、 "aeb" 和 "" (空字符串)。
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入: a = "aba", b = "cdc"
+ * 输出: 3
+ * 解释: 最长特殊序列可为 "aba" (或 "cdc")，两者均为自身的子序列且不是对方的子序列。
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：a = "aaa", b = "bbb"
+ * 输出：3
+ * 解释: 最长特殊序列是 "aaa" 和 "bbb" 。
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：a = "aaa", b = "aaa"
+ * 输出：-1
+ * 解释: 字符串 a 的每个子序列也是字符串 b 的每个子序列。同样，字符串 b 的每个子序列也是字符串 a 的子序列。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= a.length, b.length <= 100
+ * a 和 b 由小写英文字母组成
+ *
+ *
+ */
+
+/**
+ * @File    :   521.最长特殊序列-ⅰ.go
+ * @Time    :   2022/03/05 11:17:19
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+// @lc code=start
+func findLUSlength(a string, b string) int {
+	if a == b {
+		return -1
+	} else if len(a) > len(b) {
+		return len(a)
+	} else {
+		return len(b)
+	}
+}
+
+// @lc code=end
