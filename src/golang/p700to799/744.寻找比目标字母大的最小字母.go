64e499c951774225b27231d9ffaad271db28c101
wylu <158677478@qq.com>
2022-04-03 08:57:20 +0800

    寻找比目标字母大的最小字母

--- src/golang/p700to799/744.寻找比目标字母大的最小字母.go
+++ src/golang/p700to799/744.寻找比目标字母大的最小字母.go
@@ -78,7 +78,7 @@ func nextGreatestLetter(letters []byte, target byte) byte {
 	if target >= letters[len(letters)-1] {
 		return letters[0]
 	}
-	i := sort.Search(len(letters)-1, func(i int) bool { return letters[i] > target })
+	i := sort.Search(len(letters), func(i int) bool { return letters[i] > target })
 	return letters[i]
 }
 
