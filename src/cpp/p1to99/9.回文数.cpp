2e89f1a14c186d3f7efa2753acba338b9b28b1d6
wylu <158677478@qq.com>
2022-04-06 21:14:16 +0800

    回文数

---
+++ src/cpp/p1to99/9.回文数.cpp
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=cpp
+ *
+ * [9] 回文数
+ *
+ * https://leetcode-cn.com/problems/palindrome-number/description/
+ *
+ * algorithms
+ * Easy (57.44%)
+ * Likes:    1928
+ * Dislikes: 0
+ * Total Accepted:    980.8K
+ * Total Submissions: 1.7M
+ * Testcase Example:  '121'
+ *
+ * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+ * 
+ * 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * 
+ * 
+ * 例如，121 是回文，而 123 不是。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：x = 121
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：x = -121
+ * 输出：false
+ * 解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：x = 10
+ * 输出：false
+ * 解释：从右向左读, 为 01 。因此它不是一个回文数。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * -2^31 <= x <= 2^31 - 1
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你能不将整数转为字符串来解决这个问题吗？
+ * 
+ */
+
+/**
+ * @File    :   9.回文数.cpp
+ * @Time    :   2022/04/06 21:12:11
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int y = 0;
+        while (x > y) {
+            y = y * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == y || x == y / 10;
+    }
+};
+// @lc code=end
