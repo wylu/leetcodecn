19cf9568d78281404684015ed278bcf10f8ac9c9
wylu <158677478@qq.com>
2022-04-21 15:59:18 +0800

    山羊拉丁文

---
+++ src/golang/p800to899/824.山羊拉丁文.go
@@ -0,0 +1,115 @@
+package p800to899
+
+import "strings"
+
+/*
+ * @lc app=leetcode.cn id=824 lang=golang
+ *
+ * [824] 山羊拉丁文
+ *
+ * https://leetcode-cn.com/problems/goat-latin/description/
+ *
+ * algorithms
+ * Easy (65.14%)
+ * Likes:    99
+ * Dislikes: 0
+ * Total Accepted:    37.8K
+ * Total Submissions: 58.1K
+ * Testcase Example:  '"I speak Goat Latin"'
+ *
+ * 给你一个由若干单词组成的句子 sentence ，单词间由空格分隔。每个单词仅由大写和小写英文字母组成。
+ *
+ * 请你将句子转换为 “山羊拉丁文（Goat Latin）”（一种类似于 猪拉丁文 - Pig Latin 的虚构语言）。山羊拉丁文的规则如下：
+ *
+ *
+ * 如果单词以元音开头（'a', 'e', 'i', 'o', 'u'），在单词后添加"ma"。
+ *
+ *
+ * 例如，单词 "apple" 变为 "applema" 。
+ *
+ *
+ * 如果单词以辅音字母开头（即，非元音字母），移除第一个字符并将它放到末尾，之后再添加"ma"。
+ *
+ * 例如，单词 "goat" 变为 "oatgma" 。
+ *
+ *
+ * 根据单词在句子中的索引，在单词最后添加与索引相同数量的字母'a'，索引从 1 开始。
+ *
+ * 例如，在第一个单词后添加 "a" ，在第二个单词后添加 "aa" ，以此类推。
+ *
+ *
+ *
+ *
+ * 返回将 sentence 转换为山羊拉丁文后的句子。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：sentence = "I speak Goat Latin"
+ * 输出："Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：sentence = "The quick brown fox jumped over the lazy dog"
+ * 输出："heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa
+ * hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= sentence.length <= 150
+ * sentence 由英文字母和空格组成
+ * sentence 不含前导或尾随空格
+ * sentence 中的所有单词由单个空格分隔
+ *
+ *
+ */
+
+/**
+ * @File    :   824.山羊拉丁文.go
+ * @Time    :   2022/04/21 15:48:28
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+// @lc code=start
+func toGoatLatin(sentence string) string {
+	vowels := "aeiouAEIOU"
+	isVowel := func(ch byte) bool {
+		for i := 0; i < 10; i++ {
+			if vowels[i] == ch {
+				return true
+			}
+		}
+		return false
+	}
+
+	ans := []string{}
+	words := strings.Fields(sentence)
+	for i, word := range words {
+		cs := []byte(word)
+		if !isVowel(cs[0]) {
+			cs = append(cs, cs[0])
+			cs = cs[1:]
+		}
+		cs = append(cs, 'm', 'a')
+		for j := 0; j <= i; j++ {
+			cs = append(cs, 'a')
+		}
+		ans = append(ans, string(cs))
+	}
+
+	return strings.Join(ans, " ")
+}
+
+// @lc code=end
