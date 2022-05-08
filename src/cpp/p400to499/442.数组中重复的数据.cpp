3e6ed4d31002077d6e60bf420f2a5df1d79955eb
wylu <158677478@qq.com>
2022-05-08 10:27:39 +0800

    数组中重复的数据

---
+++ src/cpp/p400to499/442.数组中重复的数据.cpp
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode.cn id=442 lang=cpp
+ *
+ * [442] 数组中重复的数据
+ *
+ * https://leetcode-cn.com/problems/find-all-duplicates-in-an-array/description/
+ *
+ * algorithms
+ * Medium (72.02%)
+ * Likes:    526
+ * Dislikes: 0
+ * Total Accepted:    61.7K
+ * Total Submissions: 85.9K
+ * Testcase Example:  '[4,3,2,7,8,2,3,1]'
+ *
+ * 给你一个长度为 n 的整数数组 nums ，其中 nums 的所有整数都在范围 [1, n] 内，且每个整数出现 一次 或 两次 。请你找出所有出现
+ * 两次 的整数，并以数组形式返回。
+ * 
+ * 你必须设计并实现一个时间复杂度为 O(n) 且仅使用常量额外空间的算法解决此问题。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [4,3,2,7,8,2,3,1]
+ * 输出：[2,3]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [1,1,2]
+ * 输出：[1]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [1]
+ * 输出：[]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * n == nums.length
+ * 1 <= n <= 10^5
+ * 1 <= nums[i] <= n
+ * nums 中的每个元素出现 一次 或 两次
+ * 
+ * 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * @File    :   442.数组中重复的数据.cpp
+ * @Time    :   2022/05/08 10:12:18
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            while (nums[nums[i] - 1] != nums[i]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) ans.push_back(nums[i]);
+        }
+
+        return ans;
+    }
+};
+// @lc code=end
