c8c009fad64777b77e4b89d083b4875dc411dbd0
wylu <158677478@qq.com>
2022-08-05 21:33:28 +0800

    在二叉树中增加一行

---
+++ src/golang/p600to699/623.在二叉树中增加一行.go
@@ -0,0 +1,112 @@
+package p600to699
+
+/*
+ * @lc app=leetcode.cn id=623 lang=golang
+ *
+ * [623] 在二叉树中增加一行
+ *
+ * https://leetcode.cn/problems/add-one-row-to-tree/description/
+ *
+ * algorithms
+ * Medium (59.76%)
+ * Likes:    179
+ * Dislikes: 0
+ * Total Accepted:    34.7K
+ * Total Submissions: 58.1K
+ * Testcase Example:  '[4,2,6,3,1,5]\n1\n2'
+ *
+ * 给定一个二叉树的根 root 和两个整数 val 和 depth ，在给定的深度 depth 处添加一个值为 val 的节点行。
+ *
+ * 注意，根节点 root 位于深度 1 。
+ *
+ * 加法规则如下:
+ *
+ *
+ * 给定整数 depth，对于深度为 depth - 1 的每个非空树节点 cur ，创建两个值为 val 的树节点作为 cur
+ * 的左子树根和右子树根。
+ * cur 原来的左子树应该是新的左子树根的左子树。
+ * cur 原来的右子树应该是新的右子树根的右子树。
+ * 如果 depth == 1 意味着 depth - 1 根本没有深度，那么创建一个树节点，值 val
+ * 作为整个原始树的新根，而原始树就是新根的左子树。
+ *
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ *
+ *
+ * 输入: root = [4,2,6,3,1,5], val = 1, depth = 2
+ * 输出: [4,1,1,2,null,null,6,3,1,5]
+ *
+ * 示例 2:
+ *
+ *
+ *
+ *
+ * 输入: root = [4,2,null,3,1], val = 1, depth = 3
+ * 输出:  [4,2,null,1,1,3,null,null,1]
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 节点数在 [1, 10^4] 范围内
+ * 树的深度在 [1, 10^4]范围内
+ * -100 <= Node.val <= 100
+ * -10^5 <= val <= 10^5
+ * 1 <= depth <= the depth of tree + 1
+ *
+ *
+ */
+
+/**
+ * @File    :   623.在二叉树中增加一行.go
+ * @Time    :   2022/08/05 21:18:56
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func addOneRow(root *TreeNode, val int, depth int) *TreeNode {
+	if depth == 1 {
+		return &TreeNode{Val: val, Left: root}
+	}
+
+	que := []*TreeNode{root}
+	for i := 1; i < depth-1; i++ {
+		for j := len(que) - 1; j >= 0; j-- {
+			node := que[0]
+			que = que[1:]
+			if node.Left != nil {
+				que = append(que, node.Left)
+			}
+			if node.Right != nil {
+				que = append(que, node.Right)
+			}
+		}
+	}
+
+	for _, node := range que {
+		node.Left = &TreeNode{Val: val, Left: node.Left}
+		node.Right = &TreeNode{Val: val, Right: node.Right}
+	}
+
+	return root
+}
+
+// @lc code=end
