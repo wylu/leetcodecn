986d8d2dcc5c31dec812356f3b4e19a4e8b835a5
wylu <158677478@qq.com>
2022-08-15 22:44:05 +0800

    设计循环双端队列

---
+++ src/python/p600to699/641.设计循环双端队列.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   641.设计循环双端队列.py
+@Time    :   2022/08/15 22:28:40
+@Author  :   wylu
+@Version :   1.0
+@Contact :   15wylu@gmail.com
+@License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+@Desc    :
+"""
+
+#
+# @lc app=leetcode.cn id=641 lang=python3
+#
+# [641] 设计循环双端队列
+#
+# https://leetcode.cn/problems/design-circular-deque/description/
+#
+# algorithms
+# Medium (57.03%)
+# Likes:    178
+# Dislikes: 0
+# Total Accepted:    48.3K
+# Total Submissions: 84.6K
+# Testcase Example:
+# '["MyCircularDeque","insertLast","insertLast","insertFront","insertFront","getRear","isFull","deleteLast","insertFront","getFront"]\n'
+# + '[[3],[1],[2],[3],[4],[],[],[],[4],[]]'
+#
+# 设计实现双端队列。
+#
+# 实现 MyCircularDeque 类:
+#
+#
+# MyCircularDeque(int k) ：构造函数,双端队列最大为 k 。
+# boolean insertFront()：将一个元素添加到双端队列头部。 如果操作成功返回 true ，否则返回 false 。
+# boolean insertLast() ：将一个元素添加到双端队列尾部。如果操作成功返回 true ，否则返回 false 。
+# boolean deleteFront() ：从双端队列头部删除一个元素。 如果操作成功返回 true ，否则返回 false 。
+# boolean deleteLast() ：从双端队列尾部删除一个元素。如果操作成功返回 true ，否则返回 false 。
+# int getFront() )：从双端队列头部获得一个元素。如果双端队列为空，返回 -1 。
+# int getRear() ：获得双端队列的最后一个元素。 如果双端队列为空，返回 -1 。
+# boolean isEmpty() ：若双端队列为空，则返回 true ，否则返回 false  。
+# boolean isFull() ：若双端队列满了，则返回 true ，否则返回 false 。
+#
+#
+#
+#
+# 示例 1：
+#
+#
+# 输入
+# ["MyCircularDeque", "insertLast", "insertLast", "insertFront", "insertFront",
+# "getRear", "isFull", "deleteLast", "insertFront", "getFront"]
+# [[3], [1], [2], [3], [4], [], [], [], [4], []]
+# 输出
+# [null, true, true, true, false, 2, true, true, true, 4]
+#
+# 解释
+# MyCircularDeque circularDeque = new MycircularDeque(3); // 设置容量大小为3
+# circularDeque.insertLast(1);                    // 返回 true
+# circularDeque.insertLast(2);                    // 返回 true
+# circularDeque.insertFront(3);                    // 返回 true
+# circularDeque.insertFront(4);                    // 已经满了，返回 false
+# circularDeque.getRear();                  // 返回 2
+# circularDeque.isFull();                        // 返回 true
+# circularDeque.deleteLast();                    // 返回 true
+# circularDeque.insertFront(4);                    // 返回 true
+# circularDeque.getFront();                // 返回 4
+#
+#
+#
+#
+# 提示：
+#
+#
+# 1 <= k <= 1000
+# 0 <= value <= 1000
+# insertFront, insertLast, deleteFront, deleteLast, getFront, getRear, isEmpty,
+# isFull  调用次数不大于 2000 次
+#
+#
+#
+
+
+# @lc code=start
+class Node:
+
+    def __init__(self, val):
+        self.prev = None
+        self.next = None
+        self.val = val
+
+
+class MyCircularDeque:
+
+    def __init__(self, k: int):
+        self.head = None
+        self.tail = None
+        self.capacity = k
+        self.size = 0
+
+    def insertFront(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        node = Node(value)
+        if self.isEmpty():
+            self.head = node
+            self.tail = node
+        else:
+            node.next = self.head
+            self.head.prev = node
+            self.head = node
+        self.size += 1
+        return True
+
+    def insertLast(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        node = Node(value)
+        if self.isEmpty():
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+        self.size += 1
+        return True
+
+    def deleteFront(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.head = self.head.next
+        if self.head:
+            self.head.prev = None
+        self.size -= 1
+        return True
+
+    def deleteLast(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.tail = self.tail.prev
+        if self.tail:
+            self.tail.next = None
+        self.size -= 1
+        return True
+
+    def getFront(self) -> int:
+        return -1 if self.isEmpty() else self.head.val
+
+    def getRear(self) -> int:
+        return -1 if self.isEmpty() else self.tail.val
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        return self.size == self.capacity
+
+
+# Your MyCircularDeque object will be instantiated and called as such:
+# obj = MyCircularDeque(k)
+# param_1 = obj.insertFront(value)
+# param_2 = obj.insertLast(value)
+# param_3 = obj.deleteFront()
+# param_4 = obj.deleteLast()
+# param_5 = obj.getFront()
+# param_6 = obj.getRear()
+# param_7 = obj.isEmpty()
+# param_8 = obj.isFull()
+# @lc code=end
