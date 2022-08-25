137de483fa5c2521a4682c0fcfeccabb61d8c39f
wylu <158677478@qq.com>
2022-08-25 23:36:43 +0800

    找到-k-个最接近的元素

---
+++ src/cpp/p600to699/658.找到-k-个最接近的元素.cpp
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=658 lang=cpp
+ *
+ * [658] 找到 K 个最接近的元素
+ *
+ * https://leetcode.cn/problems/find-k-closest-elements/description/
+ *
+ * algorithms
+ * Medium (48.07%)
+ * Likes:    424
+ * Dislikes: 0
+ * Total Accepted:    71.4K
+ * Total Submissions: 148.6K
+ * Testcase Example:  '[1,2,3,4,5]\n4\n3'
+ *
+ * 给定一个 排序好 的数组 arr ，两个整数 k 和 x ，从数组中找到最靠近 x（两数之差最小）的 k 个数。返回的结果必须要是按升序排好的。
+ * 
+ * 整数 a 比整数 b 更接近 x 需要满足：
+ * 
+ * 
+ * |a - x| < |b - x| 或者
+ * |a - x| == |b - x| 且 a < b
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：arr = [1,2,3,4,5], k = 4, x = 3
+ * 输出：[1,2,3,4]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：arr = [1,2,3,4,5], k = 4, x = -1
+ * 输出：[1,2,3,4]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= k <= arr.length
+ * 1 <= arr.length <= 10^4
+ * arr 按 升序 排列
+ * -10^4 <= arr[i], x <= 10^4
+ * 
+ * 
+ */
+
+/**
+ * @File    :   658.找到-k-个最接近的元素.cpp
+ * @Time    :   2022/08/25 22:56:19
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int left = 0, right = arr.size() - k;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - arr[mid] > arr[mid + k] - x) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return vector<int>(arr.begin() + left, arr.begin() + left + k);
+    }
+};
+// @lc code=end
