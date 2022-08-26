f845532e97684febf3a6e18a8b42d7473b724a93
wylu <158677478@qq.com>
2022-08-26 12:39:02 +0800

    数组中两元素的最大乘积

---
+++ src/golang/p1400to1499/1464.数组中两元素的最大乘积.go
@@ -0,0 +1,75 @@
+package p1400to1499
+
+import "sort"
+
+/*
+ * @lc app=leetcode.cn id=1464 lang=golang
+ *
+ * [1464] 数组中两元素的最大乘积
+ *
+ * https://leetcode.cn/problems/maximum-product-of-two-elements-in-an-array/description/
+ *
+ * algorithms
+ * Easy (78.40%)
+ * Likes:    64
+ * Dislikes: 0
+ * Total Accepted:    53.5K
+ * Total Submissions: 68.2K
+ * Testcase Example:  '[3,4,5,2]'
+ *
+ * 给你一个整数数组 nums，请你选择数组的两个不同下标 i 和 j，使 (nums[i]-1)*(nums[j]-1) 取得最大值。
+ *
+ * 请你计算并返回该式的最大值。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [3,4,5,2]
+ * 输出：12
+ * 解释：如果选择下标 i=1 和 j=2（下标从 0 开始），则可以获得最大值，(nums[1]-1)*(nums[2]-1) = (4-1)*(5-1)
+ * = 3*4 = 12 。
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：nums = [1,5,4,5]
+ * 输出：16
+ * 解释：选择下标 i=1 和 j=3（下标从 0 开始），则可以获得最大值 (5-1)*(5-1) = 16 。
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：nums = [3,7]
+ * 输出：12
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 2 <= nums.length <= 500
+ * 1 <= nums[i] <= 10^3
+ *
+ *
+ */
+
+/**
+ * @File    :   1464.数组中两元素的最大乘积.go
+ * @Time    :   2022/08/26 12:37:06
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+
+// @lc code=start
+func maxProduct(nums []int) int {
+	sort.Ints(nums)
+	n := len(nums)
+	return (nums[n-1] - 1) * (nums[n-2] - 1)
+}
+
+// @lc code=end
