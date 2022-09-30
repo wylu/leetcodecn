6285ba57ac572ad4ff2ebf35adf98e98d967722c
wylu <158677478@qq.com>
2022-09-30 19:49:43 +0800

    第k个数

---
+++ src/python/interview/17.09.第k个数.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   17.09.第k个数.py
+@Time    :   2022/09/28 19:55:36
+@Author  :   wylu
+@Version :   1.0
+@Contact :   15wylu@gmail.com
+@License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+@Desc    :
+"""
+
+
+class Solution:
+
+    def getKthMagicNumber(self, k: int) -> int:
+        dp = [0] * (k + 1)
+        dp[1] = 1
+        p3 = p5 = p7 = 1
+        for i in range(2, k + 1):
+            num3, num5, num7 = dp[p3] * 3, dp[p5] * 5, dp[p7] * 7
+            dp[i] = min(num3, num5, num7)
+            if dp[i] == num3:
+                p3 += 1
+            if dp[i] == num5:
+                p5 += 1
+            if dp[i] == num7:
+                p7 += 1
+        return dp[k]
